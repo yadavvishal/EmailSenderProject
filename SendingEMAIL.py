b6f6a28505203e9b5e4d2172a220fca22728798b
yadavvishal <90525000+yadavvishal@users.noreply.github.com>
2022-04-24 20:43:00 +0530

    Add files via upload

---
+++ SendingEMAIL.py
@@ -0,0 +1,16 @@
+
+## First turn on Less secure app of google account ##
+
+
+from email import message
+import smtplib as s
+ob=s.SMTP("smtp.gmail.com",587)
+ob.starttls()
+ob.login("your email id", email password)
+subject="Sending email using python"
+body=" Hiii"
+message="Subject:{}\n{}".format(subject, body)
+listOfAddress=["receiver email id","receiver email id"]
+ob.sendmail("your email id",listOfAddress,message)
+print("Send successfully")
+ob.quit()
